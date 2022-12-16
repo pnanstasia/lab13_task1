6f559b5147b91eb593a055aafad64810db8e75d5
pnanstasia <116551880+pnanstasia@users.noreply.github.com>
2022-12-16 21:14:36 +0200

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -11,13 +11,13 @@ def linear_search(list_of_values, value):
     >>> linear_search(["flower", "sky", "grass"], "flower")
     0
     >>> linear_search([56, 89, 100, 101], 102)
-    1
+    -1
     >>> linear_search("123", 3)
     """
     if not isinstance(list_of_values, list):
         return None
     elif value not in list_of_values:
-        return 1
+        return -1
     for index, element in enumerate(list_of_values):
         if element == value:
             return index
